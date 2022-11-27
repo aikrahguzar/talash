c259fbc8dc6f876dad6e19cb91cf77281812d1f8
Rahguzar <aikrahguzar@zohomail.eu>
2022-11-27 21:53:58 +0100

    Remove unused unboxed instances + misc

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -13,7 +13,13 @@
 * FileTree is now a record.
 * Some internal changes to get it to build on somewhat older versions of ghc. Tested with 8.2.2 
 
-## 0.1.1.1 -- 20121-06-06
+## 0.1.1.1 -- 2021-06-06
 
 * Removed the spurious dependency on containers.
 * Loosened some bounds on the packages.
+
+## 0.3 -- 2022-09-25
+
+* Use a new chunked interface which gives much better code sharing and improves performance too.
+* Various internal refactors. 
+* A lot of documentation either outdated/doesn't exist as a result and needs to be revamped.
--- src/Talash/ScoredMatch.hs
+++ src/Talash/ScoredMatch.hs
@@ -10,24 +10,13 @@
 module Talash.ScoredMatch (ChunkIndex (..) , ScoredMatchSized (..) , emptyMatch) where
 
 import Talash.Intro
-import Data.Vector.Unboxed.Deriving
 import GHC.TypeNats
 import qualified Data.Vector.Unboxed.Sized as S
 
 data ChunkIndex = ChunkIndex {number :: {-# UNPACK #-} !Int , index :: {-# UNPACK #-} !Int} deriving (Eq , Ord , Show)
 
-derivingUnbox "ChunkIndex"
-    [t| ChunkIndex -> (Int , Int) |]
-    [|\(ChunkIndex i j) -> (i,j)|]
-    [|\(i,j) -> ChunkIndex i j|]
-
 data ScoredMatchSized (n::Nat) = ScoredMatchSized { score :: {-# UNPACK #-} !(Down Int), chunkIndex :: {-# UNPACK #-} !ChunkIndex
                                                   , matchData :: {-# UNPACK #-} !(S.Vector n Int)} deriving (Show)
-
-derivingUnbox "ScoredMatchSized"
-    [t| forall (n::Nat). KnownNat n => ScoredMatchSized n -> (Int , Int , Int , S.Vector n Int) |]
-    [| \(ScoredMatchSized (Down s) (ChunkIndex i j) m) -> (s, i , j , m) |]
-    [| \(s, i , j , m) -> ScoredMatchSized (Down s) (ChunkIndex i j) m |]
 
 instance Eq (ScoredMatchSized n) where
   (ScoredMatchSized s i _) == (ScoredMatchSized t j _) = s == t && i == j
